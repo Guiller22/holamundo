baae2c80cc49e7466f5cd8f3f6fed09af5815b38
Guille <guiller22>
2020-09-22 20:29:39 +0200

    prueba

---
+++ prueba.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+
+<body>
+    <?php
+    $a=12;
+    $b="9,99";
+    $c="ejemplo variables php";
+    //impresion
+    echo $a."<br>".$b."<br>".$c;
+    $mivariable="prueba  git";
+    $otra=36;
+    ?>
+</body>
+</html>
